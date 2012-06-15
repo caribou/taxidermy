91d071d8a3c9aa87515ee22d2ee86de7164cf3e1
Ryan Roemmich <ryan@weareinstrument.com>
2012-06-15 15:02:30 -0700

    update hiccup ver

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject taxidermy "0.1.0"
+(defproject taxidermy "0.1.1"
   :description "Clojure Web Forms with Java integration"
   :license {:name "MIT"
             :url "http://copyfree.org/licenses/mit/license.txt"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [hiccup "0.3.6"]])
+                 [hiccup "1.0.0"]])
