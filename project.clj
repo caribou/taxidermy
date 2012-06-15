f73c73f6cf6b6d0522fe12578065971a5ce82a75
ryan spangler <ryan.spangler@gmail.com>
2012-06-15 15:38:01 -0700

    added exclusions to project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject taxidermy "0.1.1"
+(defproject taxidermy "0.1.2"
   :description "Clojure Web Forms with Java integration"
   :license {:name "MIT"
             :url "http://copyfree.org/licenses/mit/license.txt"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [hiccup "1.0.0"]])
+                 [hiccup "1.0.0" :exclusions [org.clojure/clojure ring-serve]]])
